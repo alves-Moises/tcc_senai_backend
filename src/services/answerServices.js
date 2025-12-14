b7477fd46fbe9ea63fa981cd724ae95f4e210cd9
alves-Moises <moisesinho_ra@hotmail.com>
2025-12-13 21:23:33 -0300

    new answer Services create function

---
+++ src/services/answerServices.js
@@ -0,0 +1,41 @@
+import { v4 } from "uuid";
+import { db } from "../database/connection.js";
+
+// const getAnswerList = async
+
+const createAnswer = async ( 
+    QuestionId,
+    { 
+        text, 
+        correct 
+    } 
+) => {
+    const id = v4()
+    const query = `
+        insert into answers
+        (answer_id, text, question_id, correct)
+        values
+        ( ?, ?, ?, ?);
+    `
+    
+    await db.query(
+        query,
+        [    
+            id, 
+            text, 
+            QuestionId,
+            correct
+        ]
+    )
+    const data = await {
+        id: id,
+        text: text, 
+        correct: correct,
+    }
+
+    return data
+}
+
+export {
+    createAnswer
+}
