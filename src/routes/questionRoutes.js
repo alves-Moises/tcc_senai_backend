22f1769bb250cd16926c358f9f7c533de3463494
alves-Moises <moisesinho_ra@hotmail.com>
2025-12-14 01:35:25 -0300

    update routes

--- src/routes/questionRoutes.js
+++ src/routes/questionRoutes.js
@@ -1,10 +1,14 @@
-import { createCompleteQuestion } from "../controller/generalController.js"
+import { 
+    createCompleteQuestion,
+    getAllCompleteQuestions
+} from "../controller/generalController.js"
 import { listQuestions } from "../controller/questionController.js"
 
 
 const questionRoutes = async (app, opt) => {
     app.get("/", listQuestions),
-    app.post("/", createCompleteQuestion)
+    app.post("/", createCompleteQuestion),
+    app.get("/all", getAllCompleteQuestions)
 }
 
 
