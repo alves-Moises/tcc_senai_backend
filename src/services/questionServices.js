42eb5c2e75461b678fac16eae531fe6b88f35e66
alves-Moises <moisesinho_ra@hotmail.com>
2025-12-13 21:10:51 -0300

    create question services methods

--- src/services/questionServices.js
+++ src/services/questionServices.js
@@ -1,3 +1,35 @@
 import { v4 } from "uuid";
-// import {db}
+import {db} from "../database/connection.js"
+
+
+const getAllQuestions = async () => {
+    const questionList = await db.query("select  *  from questions")
+    return questionList
+}
+
+
+const createQuestion = async (question) => {
+    const QuestionID = v4()
+    const QuestionQuery = `
+        insert into questions
+        (question_id, question)
+        values
+        (?, ?);
+    `
+    
+        await db.query(
+        QuestionQuery, 
+        [
+            QuestionID,
+            question
+        ]
+    )
+
+    return {
+        id: QuestionID,
+        question: question
+    }
+
+}
+export { getAllQuestions, createQuestion }
 
