1dd0d2d7a33cc6664df2c81e07910898767b56a7
alves-Moises <moisesinho_ra@hotmail.com>
2025-12-14 18:57:54 -0300

    update && delete question added

--- src/controller/questionController.js
+++ src/controller/questionController.js
@@ -16,6 +16,27 @@ const addQuestion = async (question) => {
     return CreatedQuestion
 }
 
+const deleteQuestionById = async (req, res) => {
+    const removed = await deleteQuestion(req.params.id)
+
+    if(removed.affectedRows == 0){
+        return res.code(404).send("Question does not exist!")
+    }
+    return res.status(200).send(`Deleted successful.`)
+}
+
+const updateQuestionById = async (req, res) => {
+    const id = req.params.id
+    const question = req.body.question
+
+    const result = await updateQuestion(id, question)
+    if(result.affectedRows == 0){
+        return res.status(404).send("Question id doesn't exist...")
+    }
+
+    return res.status(200).send("Update successful")
+}
+
 export { 
     listQuestions,
     listCompleteQuestions,
