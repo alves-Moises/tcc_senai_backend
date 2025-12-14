e46339c5c09d6102ea70f87aa77b7211088abc52
alves-Moises <moisesinho_ra@hotmail.com>
2025-12-13 21:20:14 -0300

    update server initialization

--- src/server.js
+++ src/server.js
@@ -5,8 +5,19 @@ configDotenv()
 
 const app = buildApp()
 
-app.listen({
-    port: process.env.PORT,
-    host: process.env.HOST
-})
+app.listen(
+    {
+        port: process.env.PORT,
+        host: process.env.HOST,
+    },
+    (err, addr) => {
+        if(err){
+            app.log.error(err)
+            process.exit(1)
+        }
+
+        console.log(`Server running at ${addr}`)
+    }
+)   
+
 export {app}
