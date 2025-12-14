0f7815ba675c4d092ed4506582e693ce42b2022c
alves-Moises <moisesinho_ra@hotmail.com>
2025-12-14 18:54:56 -0300

    new routes

--- src/routes/questionRoutes.js
+++ src/routes/questionRoutes.js
@@ -2,19 +2,15 @@ import {
     createCompleteQuestion,
     getAllCompleteQuestions
 } from "../controller/generalController.js"
-
-import { 
-    deleteQuestionById, 
-    getQuestionByID, 
-    listQuestions, 
-    updateQuestionById
-} from "../controller/questionController.js"
-
+import { listQuestions } from "../controller/questionController.js"
 
 
 const questionRoutes = async (app, opt) => {
     app.get("/", listQuestions),
+    app.get("/:id", getQuestionByID),
+    app.patch("/:id", updateQuestionById),
     app.post("/", createCompleteQuestion),
+    app.delete("/:id", deleteQuestionById),
     app.get("/all", getAllCompleteQuestions)
 }
 
