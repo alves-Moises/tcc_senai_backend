b8de960ff3f4397348029a09df2d261a17f80e28
alves-Moises <moisesinho_ra@hotmail.com>
2025-12-14 19:03:52 -0300

    new: getQuestion

--- src/controller/questionController.js
+++ src/controller/questionController.js
@@ -11,30 +11,15 @@ const listCompleteQuestions = async () => {
     return questionList
 }
 
+const getQuestionByID = async (req, res) => {
+    const question = await getQuestion(req.params.id)
+
+    return question
+}
+
 const addQuestion = async (question) => {
     const CreatedQuestion = await createQuestion(question)
     return CreatedQuestion
-}
-
-const deleteQuestionById = async (req, res) => {
-    const removed = await deleteQuestion(req.params.id)
-
-    if(removed.affectedRows == 0){
-        return res.code(404).send("Question does not exist!")
-    }
-    return res.status(200).send(`Deleted successful.`)
-}
-
-const updateQuestionById = async (req, res) => {
-    const id = req.params.id
-    const question = req.body.question
-
-    const result = await updateQuestion(id, question)
-    if(result.affectedRows == 0){
-        return res.status(404).send("Question id doesn't exist...")
-    }
-
-    return res.status(200).send("Update successful")
 }
 
 const deleteQuestionById = async (req, res) => {
