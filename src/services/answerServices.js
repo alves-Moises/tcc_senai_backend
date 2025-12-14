b4066364ae996606115b93b1ad440039c30972a1
alves-Moises <moisesinho_ra@hotmail.com>
2025-12-14 18:53:23 -0300

    new: update anser service

--- src/services/answerServices.js
+++ src/services/answerServices.js
@@ -59,6 +59,22 @@ const deleteAnswer = async(
     return data    
 }
 
+const updateAnswer = async (
+    id, 
+    text
+) =>{
+    const query = `
+        update answers
+        set text = '${text}'
+        where answer_id = '${id}'
+        ;
+    `
+
+    const [data] = await db.query(query)
+    return data
+}
+
+
 export {
     createAnswer,
     getAllAnswersByQuestion
