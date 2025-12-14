3a034bbf8f9d6544631a75ac96d29f45cd1f7733
alves-Moises <moisesinho_ra@hotmail.com>
2025-12-14 01:29:39 -0300

    new: get all complete questions

--- src/controller/generalController.js
+++ src/controller/generalController.js
@@ -25,4 +25,35 @@ const createCompleteQuestion = async (req, res) => {
     return res.code(201).send(QuestionData)
 }
 
-export {createCompleteQuestion}
+const getAllCompleteQuestions = async (req, res) => {
+    const [questionList] = await listCompleteQuestions()
+    // console.log(questionList)
+    const questionListComplete = questionList.map(
+        async (
+            {
+                question_id, 
+                question
+            }
+        ) => {
+            const questText = question
+            const answerList = await getAnswerByQuestionId(question_id) 
+            // console.log(answerList)
+            
+            const data = {
+                question: questText,
+                answers: answerList[0]
+            }
+            console.log("VVVVVVV")
+            console.log(data)
+            console.log("^^^^^ ")
+            return data
+        }
+    )
+    return res.code(200).send(questionListComplete)
+}
+
+export {
+    createCompleteQuestion,
+    getAllCompleteQuestions
+
+}
