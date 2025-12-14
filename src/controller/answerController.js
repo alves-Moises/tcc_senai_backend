e6408bec20bd57569d6023c152b392c9c4720b69
alves-Moises <moisesinho_ra@hotmail.com>
2025-12-14 01:24:00 -0300

    new: getrAnswer byquestion id controller

--- src/controller/answerController.js
+++ src/controller/answerController.js
@@ -1,11 +1,24 @@
-import { createAnswer } from "../services/answerServices.js"
+import { 
+    createAnswer, 
+    getAllAnswersByQuestion 
+} from "../services/answerServices.js"
 
 const getAnswerList = async() => {
     
 }
+
+const getAnswerByQuestionId = async (id) => {
+    const answerList = await getAllAnswersByQuestion(id)
+    return answerList
+}
+
+
 const newAnswer = async (answerItem, QuestionId) => {
     const Answer = await createAnswer(QuestionId, answerItem)
     return {Answer}
 }
 
-export {newAnswer}
+export {
+    newAnswer,
+    getAnswerByQuestionId
+}
