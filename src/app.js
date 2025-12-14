2768a0ac33c877822f8cc62ca6036f356740fed8
alves-Moises <moisesinho_ra@hotmail.com>
2025-12-13 21:05:19 -0300

    add swagger

--- src/app.js
+++ src/app.js
@@ -1,11 +1,50 @@
 import cors from "@fastify/cors"
-import Fastify from "fastify"
+import Fastify, { fastify } from "fastify"
+import { questionRoutes } from "./routes/questionRoutes.js"
+import { configDotenv } from "dotenv"
+import fastifySwagger from "@fastify/swagger"
+import fastifySwaggerUi from "@fastify/swagger-ui"
 
+configDotenv()
 export const buildApp = () => {
     const app = Fastify({logger: true})
 
-    app.register(cors, {origin: "*"})
-    // app.register(routes ... )
+    app.register(
+        cors, 
+        {origin: "*"}
+    )
+
+    app.register(
+        fastifySwagger, {
+            swagger: {
+                info: {
+                    title: "QuizzApi",
+                    description: "quizzAPi docs",
+                    version: "1.0.0.1"
+                },
+                host: `http://${process.env.HOST}/${process.env.PORT}`,
+                schemes: ['http'],
+                consumes: ['application/json'],
+                produces: ['application/json']
+            }
+        }
+    )
+
+    app.register(
+        fastifySwaggerUi,
+        {
+            routePrefix: "/docs",
+            exposeRoute: true
+        }
+    )
+
+    app.register(
+        questionRoutes, 
+        {prefix: "/questions"} 
+    )
+
+
+    
     return app
 }
 
