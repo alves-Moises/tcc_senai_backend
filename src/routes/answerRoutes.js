cac0d3dbd37879b081eb68e4f561ee7148bb2cd9
alves-Moises <moisesinho_ra@hotmail.com>
2025-12-15 21:11:55 -0300

    cleancode

--- src/routes/answerRoutes.js
+++ src/routes/answerRoutes.js
@@ -1,8 +1,15 @@
 import { deleteAnswerById, updateAnswerById } from "../controller/answerController.js"
 
 const anwserRoutes = async (app, opt) => {
-    app.delete("/:id", deleteAnswerById),
-    app.patch("/:id", updateAnswerById)
+    app.delete(
+        "/:id", 
+        deleteAnswerById
+    ),
+    
+    app.patch(
+        "/:id", 
+        updateAnswerById
+    )
 }
 
 export { anwserRoutes }
