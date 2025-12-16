e4abb8edb1169b7bfa41f1e5cdfa8cbd4d061155
alves-Moises <moisesinho_ra@hotmail.com>
2025-12-16 02:00:17 -0300

    update: answer delete route swagger

--- src/routes/answerRoutes.js
+++ src/routes/answerRoutes.js
@@ -3,6 +3,12 @@ import { deleteAnswerById, updateAnswerById } from "../controller/answerControll
 const anwserRoutes = async (app, opt) => {
     app.delete(
         "/:id", 
+        {
+            schema: deleteAnswer.route,
+            response: {
+                200: deleteAnswer.response 
+            }
+        },
         deleteAnswerById
     ),
     
